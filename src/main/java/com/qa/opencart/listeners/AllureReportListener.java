95ca3a305ac61f22a5132c35d9617db85efa2a61
Vishal Pamnani <vishalpamnani@Vishals-MacBook-Air.local>
2023-10-05 13:50:56 +0530

    added Allure Listener file

--- src/main/java/com/qa/opencart/listeners/AllureReportListener.java
+++ src/main/java/com/qa/opencart/listeners/AllureReportListener.java
@@ -13,96 +13,71 @@ import io.qameta.allure.Attachment;
 
 public class AllureReportListener implements ITestListener{
 	
-	private static String getTestMethodName(ITestResult result) {
-		return result.getMethod().getConstructorOrMethod().getName();
+	private static String getTestMethodName(ITestResult iTestResult) {
+		return iTestResult.getMethod().getConstructorOrMethod().getName();
 	}
-	
+
 	// Text attachments for Allure
 	@Attachment(value = "Page screenshot", type = "image/png")
 	public byte[] saveScreenshotPNG(WebDriver driver) {
 		return ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
 	}
-	
+
 	// Text attachments for Allure
 	@Attachment(value = "{0}", type = "text/plain")
 	public static String saveTextLog(String message) {
 		return message;
 	}
-	
+
 	// HTML attachments for Allure
 	@Attachment(value = "{0}", type = "text/html")
 	public static String attachHtml(String html) {
 		return html;
 	}
-	
-	public void onStart(ITestContext testContext) {
-		System.out.println("I am in onStart method " + testContext.getName());
-		//testContext.setAttribute("WebDriver", BaseTest.getDriver());
+
+	@Override
+	public void onStart(ITestContext iTestContext) {
+		System.out.println("I am in onStart method " + iTestContext.getName());
+		//iTestContext.setAttribute("WebDriver", BasePage.getDriver());
 	}
-	
-	public void onFinish(ITestContext testContext) {
-		System.out.println("I am in onFinish method " + testContext.getName());
+
+	@Override
+	public void onFinish(ITestContext iTestContext) {
+		System.out.println("I am in onFinish method " + iTestContext.getName());
 	}
-	
-	public void onTestStart(ITestResult result) {
-		System.out.println("I am in onTestStart method " + getTestMethodName(result) + "start");
+
+	@Override
+	public void onTestStart(ITestResult iTestResult) {
+		System.out.println("I am in onTestStart method " + getTestMethodName(iTestResult) + " start");
 	}
-	
-	public void onTestSuccess(ITestResult result) {
-		System.out.println("I am in onTestSuccess method " + getTestMethodName(result) + "succeeded");
+
+	@Override
+	public void onTestSuccess(ITestResult iTestResult) {
+		System.out.println("I am in onTestSuccess method " + getTestMethodName(iTestResult) + " succeed");
 	}
-	
-	public void onTestFailure(ITestResult result) {
-		System.out.println("I am in onTestFailure method " + getTestMethodName(result) + "failed");
-		Object testClass = result.getInstance();
-		//WebDriver driver = BaseTest.getDriver();
+
+	@Override
+	public void onTestFailure(ITestResult iTestResult) {
+		System.out.println("I am in onTestFailure method " + getTestMethodName(iTestResult) + " failed");
+		Object testClass = iTestResult.getInstance();
+		//WebDriver driver = BasePage.getDriver();
 		// Allure ScreenShotRobot and SaveTestLog
-		if(DriverFactory.getDriver() instanceof WebDriver) {
-			System.out.println("Screenshot captured for test case:" + getTestMethodName(result));
+		if (DriverFactory.getDriver() instanceof WebDriver) {
+			System.out.println("Screenshot captured for test case:" + getTestMethodName(iTestResult));
 			saveScreenshotPNG(DriverFactory.getDriver());
 		}
-		// Save a log on allure
-		saveTextLog(getTestMethodName(result) + "failed and screenshot taken!");
+		// Save a log on allure.
+		saveTextLog(getTestMethodName(iTestResult) + " failed and screenshot taken!");		
 	}
-	
-	public void onTestSkipped(ITestResult result) {
-		System.out.println("I am in onTestSkipped method " + getTestMethodName(result) + "skipped");
+
+	@Override
+	public void onTestSkipped(ITestResult iTestResult) {
+		System.out.println("I am in onTestSkipped method " + getTestMethodName(iTestResult) + " skipped");
 	}
-	
-	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
-		System.out.println("Test failed but it is in defined success ratio " + getTestMethodName(result));
+
+	@Override
+	public void onTestFailedButWithinSuccessPercentage(ITestResult iTestResult) {
+		System.out.println("Test failed but it is in defined success ratio " + getTestMethodName(iTestResult));
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 
 }
